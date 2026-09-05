e463453315ab10057909a12d8b9be1b54c6b7411
Ayaan <mohapatravivan7@gmail.com>
2026-09-05 10:50:41 +0530

    Add retailer voice listing flow

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import GenerateQR from "./pages/GenerateQR";
 import Verify from "./pages/Verify";
 import NGOBoard from "./pages/NGOBoard";
 import NGOLogin from "./pages/NGOLogin";
+import FarmerVoice from "./pages/FarmerVoice";
 
 function App() {
   return (
@@ -17,18 +18,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
 
-          <Route
-            path="/ngo"
-            element={
-              <div className="min-h-screen p-10">
-                <h1 className="text-3xl font-bold">NGO Board</h1>
-              </div>
-            }
-          />
+          <Route path="/ngo" element={<NGOBoard />} />
+
+          <Route path="/ngo-login" element={<NGOLogin />} />
 
           <Route path="/retailer" element={<Retailer />} />
 
           <Route path="/my-listings" element={<MyListings />} />
+
+          <Route path="/farmer" element={<FarmerVoice />} />
 
           <Route
             path="/map"
@@ -39,20 +37,10 @@ function App() {
             }
           />
 
-          <Route
-            path="/analytics"
-            element={
-              <div className="min-h-screen p-10">
-                <h1 className="text-3xl font-bold">Analytics</h1>
-              </div>
-            }
-          />
-          <Route path="/ngo" element={<NGOBoard />} />
-          <Route path="/ngo-login" element={<NGOLogin />} />
-          <Route path="/retailer" element={<div className="p-10">Retailer</div>} />
-          <Route path="/map" element={<div className="p-10">Map</div>} />
           <Route path="/analytics" element={<Analytics />} />
+
           <Route path="/generate-qr" element={<GenerateQR />} />
+
           <Route path="/verify/:id" element={<Verify />} />
         </Routes>
       </Layout>
